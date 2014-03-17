5e65e73fa45a3bbaf9890a4190635c863133872d
Eddie Ringle <eddie@eringle.net>
2014-03-17 17:08:53 -0400

    Add CMake build system
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

---
+++ include/turfs_version.h
@@ -0,0 +1,12 @@
+#ifndef __included_turfs_h
+#define __included_turfs_h
+
+extern const char turfs_VERSION_FULL[];
+
+extern const int turfs_VERSION_MAJOR;
+extern const int turfs_VERSION_MINOR;
+extern const int turfs_VERSION_PATCH;
+
+extern const char turfs_VERSION_EXTRA[];
+
+#endif /* __included_turfs_h */
